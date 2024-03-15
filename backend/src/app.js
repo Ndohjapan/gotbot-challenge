5c2348249df9f0d3027d6d3d05554d8ed0ce70a8
Joel <ndohjoelmbj16@gmail.com>
2024-03-15 08:38:15 +0100

    fix: added the base route to the health url

--- backend/src/app.js
+++ backend/src/app.js
@@ -32,7 +32,7 @@ app.use(baseRoute + '/item', item);
 app.use(baseRoute + '/file', file);
 app.use(baseRoute + '/profile', business);
 
-app.get('/health', (req, res) => {
+app.get(baseRoute + '/health', (req, res) => {
   res.send({ health: 'Ok' });
 });
 
